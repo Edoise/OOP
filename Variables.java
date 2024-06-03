2202e948f881dedee447a5f44758f291297ab0f9
Heiswinner <130251428+Edoise@users.noreply.github.com>
2024-06-03 13:38:06 +0100

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Winner";
+        boolean alive = true;
+        if (alive == true){
+            System.out.println(name);
+        }
+        else{
+            System.out.println("Not alive");
+        }
+        
+    }
+    
+}
